50550ce8ba2d5035ad88739de224e9c9f2b0c170
crovaroto <cassio.rovaroto@gmail.com>
2020-05-04 22:56:11 +0000

    Reorganizando repositório

--- RocketSeat/frontend/ReactJs/src/App.js
+++ RocketSeat/frontend/ReactJs/src/App.js
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
+
 import Header from './components/Headers.js';
 
-function App() {
-  const [ projects, setProjects ] = useState(['Desenvolvimento de apps', 'Front-end web']);
 
-  function handleAddProjects(){
-    setProjects([...projects, `Novo projeto ${Date.now()}`])
+function App() {
+  const [ projects, setProjects ] = useState([]);
+
+  useEffect(() => {
+      api.get('projects').then(response => {
+          setProjects(response.data);
+      });
+    }, []);
+
+  async function handleAddProjects(){
+    //setProjects([...projects, `Novo projeto ${Date.now()}`])
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Cássio Rovaroto"
+    });
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return ( 
@@ -13,7 +31,7 @@ function App() {
     <> 
       <Header title="Projects">
         <ul>
-          {projects.map(project => <li key={project}>{project}</li>)}
+          {projects.map(project => <li key={project.id}>{project.title}</li>)}
         </ul>
 
         <button type="button" onClick={handleAddProjects}>Adicionar Projeto</button>
