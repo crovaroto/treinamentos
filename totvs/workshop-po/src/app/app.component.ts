f0915c54dabd03390cb7c91f55eed3d905e947ba
= <=>
2020-05-06 18:49:09 -0300

    Atualização de ambiente

---
+++ totvs/workshop-po/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { PoMenuItem } from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Pessoas', link: '/people' }
+  ];
+
+  /*private onClick() {
+    alert('Clicked in menu item')
+  }*/
+
+
+
+}
